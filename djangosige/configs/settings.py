76efa7b87e1e2879528abee4c4cc2a2b78759548
Henrique Müller <henrique@mtexpert.com.br>
2023-06-02 19:59:45 -0300

    CORREÇÕES

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(APP_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
